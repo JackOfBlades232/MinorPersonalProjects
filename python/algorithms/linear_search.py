6dac4a7369ab7b15c54ee3182c4cebef951ede2c
Petr Kiyashko <103832505+JackOfBlades232@users.noreply.github.com>
2023-01-21 09:00:02 +0200

    Delete python directory

--- python/algorithms/linear_search.py
+++
@@ -1,9 +0,0 @@
-def linear_search(arr, value):
-    for i in range(len(arr)):
-        if arr[i] == value:
-            return i
-    return -1
-
-arr = list(map(int, input().split()))
-value = int(input())
-print(linear_search(arr, value))
