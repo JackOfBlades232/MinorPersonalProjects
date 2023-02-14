654f656763b0bfe4f58f2b442a1c28763a6487ad
Petr Kiyashko <hhf232@192.168.1.4>
2023-02-14 20:09:52 +0200

    Added makefile

--- c/shootemup_v2/deps.mk
+++ c/shootemup_v2/deps.mk
@@ -1,16 +1,18 @@
 player.o: player.c player.h geom.h graphics.h colors.h utils.h
-boss.o: boss.c boss.h graphics.h geom.h colors.h utils.h
 asteroid.o: asteroid.c asteroid.h geom.h graphics.h player.h spawn.h \
- colors.h utils.h
-collisions.o: collisions.c collisions.h player.h geom.h graphics.h \
- asteroid.h spawn.h buffs.h
+  colors.h utils.h
 buffs.o: buffs.c buffs.h geom.h spawn.h player.h graphics.h colors.h \
- utils.h
+  utils.h
+boss.o: boss.c boss.h graphics.h geom.h colors.h utils.h
 spawn.o: spawn.c spawn.h geom.h utils.h
-controls.o: controls.c controls.h
-graphics.o: graphics.c graphics.h
-geom.o: geom.c geom.h
+collisions.o: collisions.c collisions.h player.h geom.h graphics.h \
+  asteroid.h spawn.h buffs.h
 colors.o: colors.c colors.h
+controls.o: controls.c controls.h
+geom.o: geom.c geom.h
+graphics.o: graphics.c graphics.h
 hud.o: hud.c hud.h player.h geom.h graphics.h colors.h
+main.o: main.c graphics.h controls.h player.h geom.h boss.h asteroid.h \
+  spawn.h buffs.h collisions.h hud.h menus.h colors.h utils.h
 menus.o: menus.c menus.h graphics.h player.h geom.h colors.h utils.h
 utils.o: utils.c utils.h
